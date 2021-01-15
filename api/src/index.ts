5b4ada6749a1d980d60f0ded7e0834ad6bbd4cd5
Francesco Virga <francescod.virga@gmail.com>
2021-01-15 00:01:16 -0500

    cli todo

--- api/src/index.ts
+++ api/src/index.ts
@@ -7,6 +7,14 @@ export default { finder, parser, downloader };
 const searchTerm = process.argv[2]
 const type = (process.argv[3] ?? "Movies") as "Movies" | "TV" 
 
+/**
+ * TODO: cli
+ * bit-finder -m "movie name" -t "type"
+ * 
+ * show a list of results, enter a number to chose one.
+ * then give the option to download directly, or copy the magnet link
+ */
+
 finder.search(searchTerm, type).then(torrents => {
     console.log(torrents[0].title);
     return finder.getMagnet(torrents[0]);
