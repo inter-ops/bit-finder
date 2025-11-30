f8c79653eeb7eef128d1183f9e256a725ed72300
Francesco Virga <francescod.virga@gmail.com>
2025-11-29 17:49:39 -0800

    Fix torrent search

--- src/clients/torrentSearch.ts
+++ src/clients/torrentSearch.ts
@@ -1,9 +1,22 @@
 import TorrentSearchApi, { Torrent as TorrentType } from "torrent-search-api";
 
-// too many providers slows search significantly, using current best
-TorrentSearchApi.enableProvider("1337x");
-TorrentSearchApi.enableProvider("Rarbg");
-TorrentSearchApi.enableProvider("Yts");
+// Works
+TorrentSearchApi.enableProvider("ThePirateBay");
+TorrentSearchApi.enableProvider("TorrentProject");
+TorrentSearchApi.enableProvider("Eztv");
+
+// TODO: broken, investigate
+// TorrentSearchApi.enableProvider("Torrentz2"); // parsing code is broken
+// TorrentSearchApi.enableProvider("Yts");
+// TorrentSearchApi.enableProvider("KickassTorrents");
+
+// Disabled
+// TorrentSearchApi.enableProvider("1337x"); // Blocked by Cloudflare
+// TorrentSearchApi.enableProvider("Limetorrents"); // Blocked by Cloudflare
+// TorrentSearchApi.enableProvider("Rarbg"); // No longer active
+
+// Disabled
+// TorrentSearchApi.enableProvider("Torrent9"); // French
 
 export type Torrent = TorrentType;
 export default TorrentSearchApi;
